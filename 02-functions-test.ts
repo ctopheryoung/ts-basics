34b52eff4d4c4ef1eaa40c69354c347d9419392a
Chris Young <ctopheryoung@gmail.com>
2023-02-22 15:01:39 -0800

    add functions

---
+++ 02-functions-test.ts
@@ -0,0 +1,7 @@
+import addNumbers, { addStrings, getName } from "./02-functions";
+
+console.log(addNumbers(1, 2));
+console.log(addStrings("a", "b"));
+console.log(addStrings("a"));
+
+console.log(getName({ first: "Chris", last: "Young" }));
