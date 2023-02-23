a19bc714044b4c5ddb490874487d2f836096af73
Chris Young <ctopheryoung@gmail.com>
2023-02-22 16:49:56 -0800

    Challenge 1 solution

--- challenges/01/interfaces.ts
+++ challenges/01/interfaces.ts
@@ -1,21 +1,34 @@
+import houses from "./houses";
+
 interface House {
-...
+  name: string;
+  planets: string | string[];
 }
 
 interface HouseWithID {
-...
+  name: string;
+  planets: string | string[];
+  id: number;
 }
 
+type FilterFunction = (house: House) => boolean;
+
 function findHouses(houses: string): HouseWithID[];
-function findHouses(
-  houses: string,
-  filter: (house: House) => boolean
-): HouseWithID[];
+function findHouses(houses: string, filter: FilterFunction): HouseWithID[];
 function findHouses(houses: House[]): HouseWithID[];
+function findHouses(houses: House[], filter: FilterFunction): HouseWithID[];
+
 function findHouses(
-  houses: House[],
-  filter: (house: House) => boolean
-): HouseWithID[];
+  input: string | House[],
+  filter?: FilterFunction
+): HouseWithID[] {
+  const houses: House[] = typeof input === "string" ? JSON.parse(input) : input;
+
+  return (filter ? houses.filter(filter) : houses).map((house) => ({
+    id: houses.indexOf(house),
+    ...house,
+  }));
+}
 
 console.log(
   findHouses(JSON.stringify(houses), ({ name }) => name === "Atreides")
