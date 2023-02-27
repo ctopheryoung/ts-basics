311597cbcbb6a8edfef7818211467b90f4746093
Chris Young <ctopheryoung@gmail.com>
2023-02-27 14:31:05 -0800

    solve filter, map

--- challenges/02/index.ts
+++ challenges/02/index.ts
@@ -6,12 +6,21 @@ function forEach<T>(items: T[], callbackFn: (v: T) => void): void {
     return undefined;
   }, undefined);
 }
-forEach([1, 2, 3], (v) => console.log(v));
+forEach([1, 2, "3"], (v) => console.log(v));
 
-function filter() {
-  // implement using reduce
+function filter<T>(items: T[], callbackFn: (v: T) => boolean): T[] {
+  return items.reduce(
+    (acc: T[], currentValue) =>
+      callbackFn(currentValue) ? [...acc, currentValue] : acc,
+    []
+  );
 }
+console.log(filter([1, 2, 3, 4, 5], (v) => v > 4));
 
-function map() {
-  // implement using reduce
+function map<T, K>(items: T[], callbackFn: (v: T) => K): K[] {
+  return items.reduce(
+    (acc: K[], currentValue) => [...acc, callbackFn(currentValue)],
+    []
+  );
 }
+console.log(map([1, 2, 3], (v) => v * 2));
