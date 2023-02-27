4b073fd3d9cd718ad2e51f980577071e7e01e823
Chris Young <ctopheryoung@gmail.com>
2023-02-27 15:18:04 -0800

    add generics with keyof

---
+++ lessons/08-generics-with-keyof.ts
@@ -0,0 +1,42 @@
+function pluck<DataType, KeyType extends keyof DataType>(
+  items: DataType[],
+  key: KeyType
+): DataType[KeyType][] {
+  return items.map((item) => item[key]);
+}
+
+const dogs = [
+  { name: "Cooper", age: 9 },
+  { name: "Elko", age: 5 },
+];
+
+console.log(pluck(dogs, "age"));
+console.log(pluck(dogs, "name"));
+
+interface BaseEvent {
+  time: number;
+  user: string;
+}
+
+interface EventMap {
+  addToCart: BaseEvent & { quantity: number; productID: string };
+  checkout: BaseEvent;
+}
+
+function sendEvent<Name extends keyof EventMap>(
+  name: Name,
+  data: EventMap[Name]
+): void {
+  console.log(name, data);
+}
+
+sendEvent("addToCart", {
+  productID: "foo",
+  user: "baz",
+  quantity: 1,
+  time: 10,
+});
+sendEvent("checkout", {
+  user: "baz",
+  time: 20,
+});
